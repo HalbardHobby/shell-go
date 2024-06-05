700c3a4bee22a43ec121b44346746ecd805d6ee7
Alejandro Espinosa <5396971+HalbardHobby@users.noreply.github.com>
2024-06-04 20:28:56 -0500

    ✨ Shell recognizes unknown commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,5 +12,7 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	fmt.Fprint(os.Stdout, strings.TrimRight(input, "\n"), ": command not found\n")
 }
