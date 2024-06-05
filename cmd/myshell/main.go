8c17b4c30c7350190f6cf01766d1341c1b1c8efc
Alejandro Espinosa <5396971+HalbardHobby@users.noreply.github.com>
2024-06-04 20:35:13 -0500

    ✨ Naive implementation for REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,11 +8,14 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	// Naive implementation for REPL cycle
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	fmt.Fprint(os.Stdout, strings.TrimRight(input, "\n"), ": command not found\n")
+		fmt.Fprint(os.Stdout, strings.TrimRight(input, "\n"), ": command not found\n")
+	}
 }
